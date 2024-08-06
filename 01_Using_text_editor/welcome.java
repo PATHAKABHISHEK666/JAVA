2de071ed81963978fb5a14121abb748577caa631
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-08-06 20:53:09 +0530

    Welcome to Java Programming Language
    Completed First Video Learnt How to Compile and run code using terminal and write edit using text editor

---
+++ 01_Using_text_editor/welcome.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+
+public class welcome {
+
+	public static void main(String[] args) {
+
+		System.out.println("WELCOME TO JAVA PROGRAMMING \n");
+		System.out.print("This ");
+		System.out.println("is ");
+		System.out.println("first ");
+		System.out.print("tutorial ");
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
