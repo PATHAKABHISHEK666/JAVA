d2800d2d38032ffff75b6f98365eb2ee5c1f432f
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-08-07 21:55:35 +0530

    Completed Swapping Number Challenge

---
+++ 09_Challenge/swap.java
@@ -0,0 +1,18 @@
+
+public class swap {
+  public static void main(String[] args) {
+
+    int num1 = 12;
+    int num2 = 15;
+    System.out.println("num1: " + num1);
+    System.out.println("num2: " + num2);
+
+    System.out.println("\nSWAPPED---------------------->\n");
+
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    System.out.println("num1: " + num1);
+    System.out.println("num2: " + num2);
+
+  }
+}
