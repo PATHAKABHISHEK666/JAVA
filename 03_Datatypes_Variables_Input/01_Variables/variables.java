8537ad278eeb3a5a869c99944b4707913123696e
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-08-07 19:20:13 +0530

    Learnt  VARIABLES

---
+++ 03_Datatypes_Variables_Input/01_Variables/variables.java
@@ -0,0 +1,31 @@
+
+public class variables {
+
+  public static void main(String[] args) {
+
+    int myNumber = 12;
+
+    int yourNumber;
+    yourNumber = 122;
+    System.out.println(yourNumber);
+    yourNumber = 1234;
+    System.out.println(yourNumber);
+
+    float myFloat = 5;
+    System.out.println(myFloat);
+
+    double myDouble = 23.23232;
+    System.out.println(myDouble);
+
+    boolean isVegetarian = true;
+    System.out.println(isVegetarian);
+
+    String wishes = "GOOD MORNING";
+    System.out.println(wishes);
+
+    char myCharacter = 'K';
+    System.out.println(myCharacter);
+
+  }
+
+}
