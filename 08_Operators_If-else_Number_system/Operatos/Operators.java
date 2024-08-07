0cd2b908ad132541c2203b21d6f3ab2c82b08f01
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-08-07 21:55:12 +0530

    LEARNT OPERATORS

---
+++ 08_Operators_If-else_Number_system/Operatos/Operators.java
@@ -0,0 +1,13 @@
+
+public class Operators {
+  public static void main(String[] args) {
+
+    // ASSIGNMENT OPERATOR
+
+    int myInt = 99;
+    System.out.println(myInt);
+    int myNewInt = myInt;
+    System.out.println(myNewInt);
+
+  }
+}
