edeae379fa2b5302ddf28bbe7ee5e8ab421a0bf7
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-08-07 19:42:19 +0530

    Completed Challenge 2

---
+++ 05_Challenge/Task3.java
@@ -0,0 +1,15 @@
+
+// Right Half Pyramid
+
+public class Task3 {
+
+  public static void main(String[] args) {
+
+    System.out.println("*\n* *\n* * *\n* * * *\n* * * * *\n* * * * * *\n");
+
+    System.out.println("* * * * * *\n* * * * *\n* * * *\n* * *\n* *\n*\n");
+
+    System.out.println("      *\n     **\n    ***\n   ****\n  *****\n");
+  }
+
+}
