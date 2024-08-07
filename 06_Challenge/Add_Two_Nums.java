aafa1d5af891654a61056a628b73b9c40687c889
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-08-07 20:26:13 +0530

    Completed CHallenge to add two numbers

---
+++ 06_Challenge/Add_Two_Nums.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Add_Two_Nums {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter The First Number: ");
+    int number1 = input.nextInt();
+
+    System.out.println("Enter The Second Number: ");
+    int number2 = input.nextInt();
+
+    int sum = number1 + number2;
+
+    System.out.print("The Sum Of " + number1 + " and " + number2 + " = " + sum);
+    // System.out.print(sum);
+  }
+}
