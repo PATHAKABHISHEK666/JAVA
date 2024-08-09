8606eeb51e6053f16244ee57da848867225b1f65
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-08-09 08:14:44 +0530

    Learnt Working Of Arithmetic Operators in java

--- 08_Operators_If-else_Number_system/Operatos/Operators.java
+++ 08_Operators_If-else_Number_system/Operatos/Operators.java
@@ -4,10 +4,33 @@ public class Operators {
 
     // ASSIGNMENT OPERATOR
 
-    int myInt = 99;
-    System.out.println(myInt);
-    int myNewInt = myInt;
-    System.out.println(myNewInt);
+    /*
+     * int myInt = 99;
+     * System.out.println(myInt);
+     * int myNewInt = myInt;
+     * System.out.println(myNewInt);
+     */
+
+    // ARITHMETIC OPERATORS
+
+    int a = 19;
+    int b = 9;
+
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println(a % b);
+
+    System.out.print("\n_______________________________________\n\n");
+
+    double x = 5;
+    double y = 3;
+
+    System.out.println(x + y);
+    System.out.println(x - y);
+    System.out.println(x * y);
+    System.out.println(x / y);
 
   }
 }
