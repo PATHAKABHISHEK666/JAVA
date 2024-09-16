96ffd0d4e9a379347931b46f97f18a6467028a09
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-09-16 12:40:22 +0530

    Completed While Loops

--- 16_While_Loops/while_loops.java
+++ 16_While_Loops/while_loops.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class while_loops {
+  public static void main(String[] args) {
 
+    Scanner times = new Scanner(System.in);
+    System.out.println("Enter The Number of Times!");
+
+    int time = times.nextInt();
+
+    while (time > 0) {
+      System.out.println("Hello While Loop");
+      time--;
+    }
+
+  }
 }
